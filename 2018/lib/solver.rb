e63baf1e024280ffe99ff3873f4f3a139a9e1f93
Marty Ma <marty.ma@fluxfederation.com>
2018-12-14 17:28:33 +1100

    Day 01

--- 2018/lib/solver.rb
+++ 2018/lib/solver.rb
@@ -1,13 +1,12 @@
+def assert(actual, expected)
+  return if actual == expected
+  raise "assertion failure: expected: #{expected.inspect}\nactual: #{actual.inspect}"
+end
+
 class Solver
   attr_reader :input
 
-  def initialize(input)
+  def initialize(input = nil)
     @input = input
-  end
-
-  private
-
-  def asset(actual, expected)
-    raise "assertion failure: expected: #{expected.inspect}\nactual: #{actual.inspect}"
   end
 end
