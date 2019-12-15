85ea80da340efb00ca2809ca7aeea39f0971b9c4
Marty <mytram2@gmail.com>
2019-12-16 08:42:05 +1100

    2019 - Fix bugs in day06 - a

--- 2019/day06.rb
+++ 2019/day06.rb
@@ -1,49 +1,66 @@
 #
 
 class Solver
+  CENTER_OF_MASS = 'COM'
+
   def solve_a(direct_orbits)
     direct_orbit_map = build_direct_orbit_map(direct_orbits)
     full_orbit_map = build_full_orbit_map(direct_orbit_map)
+
+    return unless verified_full_orbit_map?(full_orbit_map)
+
     full_orbit_map.sum { |_, orbits| orbits.size }
   end
 
-  private
+  def solve_b
+    direct_orbit_map = build_direct_orbit_map(direct_orbits)
+    full_orbit_map = build_full_orbit_map(direct_orbit_map)
 
-  def build_direct_orbit_map(direct_orbits)
-    orbit_map = {}
-
-    direct_orbits.each do |orbit|
-      orbitee, orbiter = orbit.split(')').map(&:strip)
-      orbit_map[orbiter] = orbitee
-    end
-
-    orbit_map
-  end
-
-  def build_full_orbit_map(direct_orbit_map)
-    full_orbit_map = {}
-    direct_orbit_map.keys.each do |direct_orbiter|
-      build_full_orbits(direct_orbiter,
-                        direct_orbit_map,
-                        full_orbit_map)
-    end
+    return unless verified_full_orbit_map?(full_orbit_map)
 
     full_orbit_map
   end
 
-  def build_full_orbits(orbiter, direct_orbit_map, full_orbit_map)
+  private
+
+  def verified_full_orbit_map?(full_orbit_map)
+    return false if full_orbit_map.nil?
+
+    if full_orbit_map.find { |_, values| values.last != CENTER_OF_MASS }
+      puts "[X] all orbits must end at COM"
+      return false
+    end
+
+    true
+  end
+
+  def build_direct_orbit_map(direct_orbits)
+    direct_orbits.each_with_object({}) do |orbit, orbit_map|
+      orbitee, orbiter = orbit.split(')')
+      orbit_map[orbiter] = orbitee
+    end
+  end
+
+  def build_full_orbit_map(direct_orbit_map)
+    direct_orbit_map.keys.each_with_object({}) do |orbiter, orbit_map|
+      build_full_orbits(orbiter,
+                        direct_orbit_map,
+                        orbit_map)
+    end
+  end
+
+  def build_full_orbits(orbiter, direct_orbit_map, orbit_map)
+    return if orbit_map[orbiter] || orbiter == CENTER_OF_MASS
+
     orbitee = direct_orbit_map[orbiter]
-    return if orbitee.nil?
 
-    full_orbit_map[orbiter] ||= [orbitee]
+    orbit_map[orbiter] ||= [orbitee]
 
-    return if direct_orbit_map[orbitee].nil?
-
-    build_full_orbits(direct_orbit_map[orbitee],
+    build_full_orbits(orbitee,
                       direct_orbit_map,
-                      full_orbit_map)
+                      orbit_map)
 
-    full_orbit_map[orbiter] = (full_orbit_map[orbiter] + (full_orbit_map[orbitee] || [])).uniq
+    orbit_map[orbiter] = orbit_map[orbiter] + (orbit_map[orbitee] || [])
   end
 end
 
@@ -63,6 +80,7 @@ orbits = ['COM)B',
 
 orbits = File.open('data/day06.txt')
            .readlines
+           .map(&:strip)
            .reject { |item| item.strip.empty? }
 
-print solver.solve_a(orbits)
+print solver.solve_a(orbits) # 314702
